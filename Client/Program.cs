3292b5ca81b4ae24550e9e6f24853acc50cffb66
AndreyVoytsik <andrey.voytsik@gmail.com>
2018-12-24 05:45:26 +0300

    added application

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        //создаем сокет для сетевого взаимодействия
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            //подключаем сокет для передачи
+            socket.Connect("127.0.0.1", 8091);
+            //вводим сообщение
+            string message = Console.ReadLine();
+            //так как обмен проиходит байтами, преобразуем методом энкодинг нашу строку
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            //отправка сообщения
+            socket.Send(buffer);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ TCPIPTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        //создаем сокет для сетевого взаимодействия
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            //привязываем сервер к адресу
+            socket.Bind(new IPEndPoint(IPAddress.Any, 8091));
+            //определяем макс количество подключений
+            socket.Listen(5);
+            //принимаем подключения
+            Socket client = socket.Accept();
+            //Узнаем про подключение
+            Console.WriteLine("Connected");
+            //массив для хранения данных
+            byte[] buffer = new byte[1024];
+            //Получаем данные
+            client.Receive(buffer);
+            //выводим на экран, предварительно переведя из байт
+            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+            Console.ReadLine();
+        }
+    }
+}
